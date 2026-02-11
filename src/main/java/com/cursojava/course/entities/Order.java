50949f71c4967694c2dcea344ef6db5c05e1a0a5
Joao Molina <molina.joaov@gmail.com>
2026-02-11 13:24:43 -0300

    Order, Instant, ISO 8601 Part 2

--- src/main/java/com/cursojava/course/entities/Order.java
+++ src/main/java/com/cursojava/course/entities/Order.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +24,7 @@ public class Order implements Serializable{
 	private Long id;
 	private Instant moment;
 	
+	@JsonIgnore
  	@ManyToOne //anotação parao JPA entender que é uma chave estrangeira
 	@JoinColumn(name = "client_id")
  	private User client;
---
+++ src/main/java/com/cursojava/course/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.cursojava.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursojava.course.entities.Order;
+import com.cursojava.course.repositories.OrderReposity;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderReposity repository;
+
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+
+	public Order findById(Long id) {// recupera um Usuario por ID
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
