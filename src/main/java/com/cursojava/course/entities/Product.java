c5531cb0f104e727589139a2132d38839da7a5af
Joao Molina <molina.joaov@gmail.com>
2026-02-12 14:48:42 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/cursojava/course/entities/Product.java
+++ src/main/java/com/cursojava/course/entities/Product.java
@@ -9,6 +9,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -26,7 +29,10 @@ public class Product implements Serializable{
 	
 	//para garantir que o mesmo produto tenha a mesma categoria mais de uma vez
 	//por isso usar Set em vez de List
-	
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", 
+	joinColumns = @JoinColumn(name = "product_id"), 
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();//para garantir que a coleção nao comece nula e sim vazia e instanciada
 
 	public Product() {
