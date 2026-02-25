ca7a8dd9fe0a2d83f0201fd8721d6fad87e66de7
Joao Molina <molina.joaov@gmail.com>
2026-02-25 12:23:40 -0300

    Payment, one-to-one association

--- src/main/java/com/cursojava/course/config/TestConfig.java
+++ src/main/java/com/cursojava/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.cursojava.course.entities.Category;
 import com.cursojava.course.entities.Order;
 import com.cursojava.course.entities.OrderItem;
+import com.cursojava.course.entities.Payment;
 import com.cursojava.course.entities.Product;
 import com.cursojava.course.entities.User;
 import com.cursojava.course.entities.enums.OrderStatus;
@@ -81,6 +82,12 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+	
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		//como o payment é uma classe dependente do order, então para ser salvo é necessário chamar setPayment e o orderRepository
+		orderRepository.save(o1);
+	
 	}
 	
 	
